3faa14e01aacafbe499ac0becd2dcb20ece1bc94
duantruong <54516233+duantruong@users.noreply.github.com>
2019-10-02 13:12:07 -0700

    Update Frame.java

--- Frame.java
+++ Frame.java
@@ -1,20 +1,37 @@
 package Lab2;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
+
+import Exam.CircleFrame;
 
 public class Frame
 {
 	public static void main(String[] args)
 	   {
+	
+		
 	      JFrame frame = new JFrame();
 	      frame.setSize(200,250);
 
 	      JButton redButton = new JButton("Repainted red");
 	      JButton blueButton = new JButton("Repainted Blue");
 	      JButton greenButton=new JButton("Repainted green");
-	      
+	      ActionListener actionListener = new ActionListener() {
+	    		CircleFrame paintGUI;
+	            public void actionPerformed(ActionEvent e) {
+	                    if (e.getSource() == greenButton) {
+	                            paintGUI.green();
+	                    } else if (e.getSource() == redButton) {
+	                            paintGUI.red();
+	                    } else if (e.getSource() == blueButton) {
+	                            paintGUI.blue();
+	                    }
+	            }
+	    };
 	    
 	      
 	      
@@ -23,7 +40,7 @@ public class Frame
 	      frame.add(redButton);
 	      frame.add(blueButton);
 	      frame.add(greenButton);
-	      new MarsIcon(50);
+	     
 	      
 
 	      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
