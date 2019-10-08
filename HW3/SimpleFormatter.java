9705e66213f904db1dfc03ae1cb9f2c6696cd822
duantruong <54516233+duantruong@users.noreply.github.com>
2019-10-08 10:21:04 -0700

    Update SimpleFormatter.java

--- HW3/SimpleFormatter.java
+++ HW3/SimpleFormatter.java
@@ -2,26 +2,27 @@ package Invoice;
 
 /**
 A simple invoice formatter.
-*/
+ */
 public class SimpleFormatter implements InvoiceFormatter
 {
-public String formatHeader()
-{
-   total = 0;
-   return "     I N V O I C E\n\n\n";
-}
+	Invoice invoice=new Invoice();
+	public String formatHeader()
+	{
+		total = 0;
+		return "     I N V O I C E\n\n\n";
+	}
 
-public String formatLineItem(LineItem item)
-{
-   total += item.getPrice();
-   return (String.format(
-         "%s: x$%.2f\n",item.toString(),item.getPrice()));
-}
+	public String formatLineItem(LineItem item)
+	{
+		total += item.getPrice();
+		return (String.format(
+				"%s: x"+Invoice.getQuantity()+"$%.2f\n",item.toString(),total));
+	}
 
-public String formatFooter()
-{
-   return (String.format("\n\nTOTAL DUE: $%.2f\n", total));
-}
+	public String formatFooter()
+	{
+		return (String.format("\n\nTOTAL DUE: $%.2f\n", total));
+	}
 
-private double total;
+	private double total;
 }
