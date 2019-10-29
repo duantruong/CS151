030be77317a07ba2e96b3aaa72e99d5891de7ba4
duantruong <54516233+duantruong@users.noreply.github.com>
2019-10-28 17:50:59 -0700

    Create dumpArray.java

---
+++ dumpArray.java
@@ -0,0 +1,12 @@
+
+public class dumpArray
+{
+
+    public static void dumpArray(Object[] objects)
+    {
+        for(int i = 0; i < objects.length; i++)
+        {
+            System.out.println(objects[i].toString());
+        }
+    }
+}    
